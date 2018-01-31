a54d2841f4cba05a1220bba7ff11d71be9ad68cb
josediazse <jediazse@gmail.com>
2018-01-31 00:04:02 -0500

    Cleanup evenOdd implementation.

--- src/test/java/com/epi/ArrayTest.java
+++ src/test/java/com/epi/ArrayTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,12 +17,9 @@ public class ArrayTest {
 
     @Test
     public void evenOdd() {
-        final Integer[] expected = new Integer[]{2, 4, 6, 8, 9, 11, 1, 13, 15};
+        List<Integer> expected = Arrays.asList(8, 2, 4, 6, 15, 11, 13, 9, 1);
         List<Integer> input = Arrays.asList(1, 2, 4, 11, 15, 6, 8, 13, 9);
         array.evenOdd(input);
-        for (Integer integer : input) {
-            System.out.println(integer);
-        }
-        Assert.assertTrue(Arrays.equals(expected, input.toArray()));
+        Assert.assertTrue(expected.equals(input));
     }
 }
